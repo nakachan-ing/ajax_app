38286c74ed89a6289766ed6ce508ffe3957e88c5
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2021-03-27 16:46:42 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  # get 'posts', to: 'posts#index'
+  root to: "posts#index"
   post 'posts', to: 'posts#create'
 end
